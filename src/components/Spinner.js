0efb418cf0197fe9ebe7a1fda3add222f677b83d
Dshivamthakur <88136860+Dshivamthakur@users.noreply.github.com>
2023-10-30 05:04:51 +0530

    NewsMonkey code for function based component

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-import styles from './Spinner.module.css'; 
+import styles from './Spinner.module.css';
 
-export default class Spinner extends Component {
-  render() {
-    return (
-        <div className={`${styles.center}`}>
-            <div className={`${styles.loader} `}></div>
-        </div>
-        
-    )
-  }
+const Spinner = () => {
+  return (
+    <div className={`${styles.center}`}>
+      <div className={`${styles.loader} `}></div>
+    </div>
+
+  )
 }
+
+export default Spinner
